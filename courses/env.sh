b0293d44a99a6af98b323eec30f08e7d1abed857
MelvinKim <melobruk1@gmail.com>
2023-05-13 19:53:58 +0300

    feat: database operations for courses microservice

---
+++ courses/env.sh
@@ -0,0 +1,8 @@
+export ENVIRONMENT='test'
+export DB_HOST='localhost'
+export DB_USER='postgres'
+export DB_PASSWORD='postgres'
+export DB_NAME='sudocode'
+export DB_PORT='5432'
+
+export TEST_DB_NAME='sudocode_test'
